4c5f9173e6a4702873ecee52f0755b6e3884541e
Landon Adrian <landon.s.adrian@gmail.com>
2015-08-03 21:05:33 -0400

    database recipe fixed with deploy user

--- cookbook/web-server/metadata.rb
+++ cookbook/web-server/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'voxes@narshfar.com'
 license          'All rights reserved'
 description      'Installs/Configures Webserver'
 long_description 'Installs/Configures Webserver'
-version          '0.2.4'
+version          '0.2.5'
 
 depends 'apt'
 depends 'rbenv'
